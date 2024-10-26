37e7f26ce7250fae3653ac1fdfe0ee50a26f524d
Ginna-Lasluisa <e1725177909@uisrael.edu.ec>
2024-10-26 07:47:33 -0500

    Update Resumen.xaml.cs

--- Views/Resumen.xaml.cs
+++ Views/Resumen.xaml.cs
@@ -1,13 +1,32 @@
-public partial class SummaryPage : ContentPage
-{
-    public SummaryPage()
-    {
-        InitializeComponent();
-        // Asignar datos a los bindings
-    }
+using Microsoft.Maui.Controls;
 
-    private async void OnLogoutClicked(object sender, EventArgs e)
+namespace glasluisaExamen.Views
+{
+    public partial class Resumen : ContentPage
     {
-        await Navigation.PopToRootAsync(); // Vuelve a la ventana de login
+        public Resumen(string usuario, string nombre, string apellido, string va, DateTime fecha, string ciudad, string montoInicial, string cuotaMensual)
+        {
+            InitializeComponent();
+            lblUsuarioConectado.Text = usuario;
+            lblNombre.Text = nombre;
+            lblApellido.Text = apellido;
+            lblVA.Text = va;
+            lblFecha.Text = fecha.ToShortDateString();
+            lblCiudad.Text = ciudad;
+            lblMontoInicial.Text = montoInicial;
+            lblCuotaMensual.Text = cuotaMensual;
+
+            decimal monto = 300m;
+            decimal pagoInicial = monto * 0.15m; 
+            decimal saldoRestante = monto - pagoInicial;
+            decimal total = pagoInicial + ((saldoRestante * 1.05m) * 3);
+
+            lblPagoTotal.Text = total.ToString("0.00");
+        }
+
+        private async void btnCerrarSesion_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
     }
 }
