9fcc885e552ac02fb73df0ea86990d3e8d8f1623
Ginna-Lasluisa <e1725177909@uisrael.edu.ec>
2024-10-27 11:15:20 -0500

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace glasluisaExamen
 {
     public partial class App : Application
@@ -6,8 +8,8 @@ namespace glasluisaExamen
         {
             InitializeComponent();
 
+            // Establece la página principal de la aplicación como una NavigationPage
             MainPage = new NavigationPage(new Views.Login());
         }
     }
 }
-
